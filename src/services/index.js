1cc974dc899fb59ef063dc999d7d4f56faeee32e
Beomsu <gobeumsu@gmail.com>
2024-03-12 13:28:04 +0900

    init : project

--- src/services/index.js
+++
@@ -1,4 +0,0 @@
-export default async function routes (fastify, options) {
-  fastify.register(import('./ping/index.js'), { prefix: '/ping' })
-  fastify.register(import('./headers/index.js'), { prefix: '/headers' })
-}
