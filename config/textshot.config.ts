70d88f90dcb7ac6b15c124549edb0b813ea8a399
Beomsu <gobeumsu@gmail.com>
2024-03-12 15:53:36 +0900

    feat : front로부터 받은 google Oauthcallback

---
+++ config/textshot.config.ts
@@ -0,0 +1,6 @@
+const isProd = process.env.NODE_ENV === "production";
+const isLocal = process.env.NODE_ENV === "local";
+
+import textShot from "./textshot.json";
+
+export {textShot};
--- src/routes/auth/google.ts
+++ src/routes/auth/google.ts
@@ -1,6 +1,12 @@
-import { FastifyInstance } from "fastify";
+import {textShot} from "config/textshot.config";
+import {FastifyInstance, FastifyReply, FastifyRequest} from "fastify";
+import {OAuth2Client} from "google-auth-library";
 
+const _google = textShot.oauth.google;
+const oAuth2Client = new OAuth2Client(_google.clientId, _google.clientSecret, "postmessage");
 
 export default async function (fastify: FastifyInstance) {
-    
+  fastify.get("/callback", async (req: FastifyRequest<{Body: {code: any}}>, res: FastifyReply) => {
+    const {tokens} = await oAuth2Client.getToken(req.body.code);
+  });
 }
