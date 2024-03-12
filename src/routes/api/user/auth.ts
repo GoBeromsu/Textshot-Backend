bb7b5e571d1e829462302a88b7e6a76563e402fc
Beomsu <gobeumsu@gmail.com>
2024-03-12 15:53:55 +0900

    feat : api/v1 경로

---
+++ src/routes/api/user/auth.ts
@@ -0,0 +1,2 @@
+import {FastifyInstance} from "fastify";
+export default async function (fastify: FastifyInstance) {}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,5 +4,6 @@ import api from "./api";
 import auth from "./auth";
 
 export default async function (fastify: FastifyInstance) {
+  fastify.register(api, {prefix: "/api/v1"});
   fastify.register(auth, {prefix: "/auth"});
 }
